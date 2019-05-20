9de5efadf9cd6ae8b86e37da8b7c7b65d3058609
Tim Lichtenberg <lichtenberg.tim@gmail.com>
2019-05-20 09:37:36 +0100

    Call SPIDER using subprocess, write output

--- RadInteriorCoupler.py
+++ RadInteriorCoupler.py
@@ -7,28 +7,26 @@ import numpy as np
 # import GGRadConv
 import SocRadConv
 import ReadInterior
+import subprocess
 
-# placeholder, interior code will produce surfaceT
-# surfaceT = 300.0*np.ones(1)  # interior code goes here
-# np.savetxt('surfaceT.txt', surfaceT)
+# run SPIDER
+subprocess.call(["spider", "-options_file", "bu_input.opts"])
 
 # save surface T to file
 ReadInterior.write_surface_quantitites()
 
 # load in surface temperature
 surfaceT = np.loadtxt('surfaceT.dat')
-# print(surfaceT)
-time_current = surfaceT[-1][0]  # K
+time_current = surfaceT[-1][0]      # K
 surfaceT_current = surfaceT[-1][1]  # K
 
 # load in volatiles released from interior
 volatiles_out = np.loadtxt('volatiles_out.dat')
-# print(volatiles_out)
 h2o_current = volatiles_out[-1][1]  # kg
 co2_current = volatiles_out[-1][2]  # kg
 
 # print current values
-print("time T_surf  h2o co2")
+print("time – T_surf – h2o – co2")
 print(time_current, surfaceT_current, h2o_current, co2_current)
 
 # calculate OLR flux given surface T
