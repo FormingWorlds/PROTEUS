3cb7d8d441fcd1c433be02bbd96099560ec6a325
Tim Lichtenberg <lichtenberg.tim@gmail.com>
2019-07-03 15:56:17 +0100

    Rudimentary coupling works

--- RadInteriorCoupler.py
+++ RadInteriorCoupler.py
@@ -14,25 +14,37 @@ from natsort import natsorted #https://pypi.python.org/pypi/natsort
 
 # SPIDER start input options
 ic_filename         = "0.json"      # JSON file to read in initial condition
-SURFACE_BC          = "4"           # 4: constant heat flux boundary condition
-SOLVE_FOR_VOLATILES = "1"           # track evolution of volatiles in interior/atmosphere reservoirs
-H2O_poststep_change = "0.05"        # fractional change in melt phase H2O concentration that triggers event
-CO2_poststep_change = "0.05"        # as above for CO2, also a 5% change to trigger an event
-nstepsmacro         = "50"          # number of timesteps
-dtmacro             = "20000"       # delta time per macrostep to advance by, in years
-heat_flux           = "1.0E10"      # prescribed start surface heat flux (e.g., 10^4 W/m^2)
+SURFACE_BC          = "4"            # 4: constant heat flux boundary condition
+SOLVE_FOR_VOLATILES = "1"            # track evolution of volatiles in interior/atmosphere reservoirs
+H2O_poststep_change = "0.50"         # fractional change in melt phase H2O concentration that triggers event
+CO2_poststep_change = "0.50"         # as above for CO2, also a 5% change to trigger an event
+nstepsmacro         = "50"           # number of timesteps
+dtmacro             = "20000"        # delta time per macrostep to advance by, in years
+heat_flux           = "1.0E4"        # prescribed start surface heat flux (e.g., 10^4 W/m^2)
 
 # Restart flag
-start_condition = "1"               # 1: Start from beginning, 2: Restart from file
+start_condition     = "1"            # 1: Start from beginning, 2: Restart from file
 
 # Total runtime
 time_current = 0
 time_max     = 1000000
 
+# Start SPIDER to generate first output file
+call_sequence = [ "spider", "-options_file", "bu_input.opts", "-initial_condition", "1", "-ic_filename", "output/"+ic_filename, "-SURFACE_BC", SURFACE_BC, "-surface_bc_value", heat_flux, "-SOLVE_FOR_VOLATILES", SOLVE_FOR_VOLATILES, "-activate_rollback", "-activate_poststep", "-H2O_poststep_change", H2O_poststep_change, "-CO2_poststep_change", CO2_poststep_change, "-nstepsmacro", "0", "-dtmacro", "1" ]
+print(call_sequence)
+subprocess.call(call_sequence)
+
+# Set restart flag
+start_condition     = "2"
+
 while time_current < time_max:
 
-    # Start SPIDER
-    subprocess.call([ "spider", "-options_file", "bu_input.opts", "-initial_condition", start_condition, "-ic_filename", "output/"+ic_filename, "-SURFACE_BC", SURFACE_BC, "-surface_bc_value", heat_flux, "SOLVE_FOR_VOLATILES", SOLVE_FOR_VOLATILES, "activate_rollback", "activate_poststep", "-H2O_poststep_change", H2O_poststep_change, "-CO2_poststep_change", CO2_poststep_change, "-nstepsmacro", nstepsmacro, "-dtmacro", dtmacro ])
+    # SPIDER restart call sequence
+    call_sequence = [ "spider", "-options_file", "bu_input.opts", "-initial_condition", start_condition, "-ic_filename", "output/"+ic_filename, "-SURFACE_BC", SURFACE_BC, "-surface_bc_value", heat_flux, "-SOLVE_FOR_VOLATILES", SOLVE_FOR_VOLATILES, "-activate_rollback", "-activate_poststep", "-H2O_poststep_change", H2O_poststep_change, "-CO2_poststep_change", CO2_poststep_change, "-nstepsmacro", nstepsmacro, "-dtmacro", dtmacro ]
+    print(call_sequence)
+
+    # Restart SPIDER
+    subprocess.call(call_sequence)
 
     # save surface T to file
     ReadInterior.write_surface_quantitites()
@@ -60,5 +72,5 @@ while time_current < time_max:
     print("time – T_surf – h2o – co2 – heat_flux – last_file")
     print(time_current, surfaceT_current, h2o_current, co2_current, heat_flux, ic_filename)
 
-    # Set restart flag
-    start_condition = "2"
+    # Set to str for subprocess
+    heat_flux = str(heat_flux)
