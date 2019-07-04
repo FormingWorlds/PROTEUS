7fa806ad0327e5d09d92ea662a99dac332c1cfce
Tim Lichtenberg <lichtenberg.tim@gmail.com>
2019-07-04 08:39:38 +0100

    Output more informative, no T-P show() during runtime

--- RadInteriorCoupler.py
+++ RadInteriorCoupler.py
@@ -14,35 +14,45 @@ import math
 from natsort import natsorted #https://pypi.python.org/pypi/natsort
 
 # SPIDER start input options
-ic_filename         = "0.json"      # JSON file to read in initial condition
+ic_filename         = "58796.json"      # JSON file to read in initial condition
 SURFACE_BC          = "4"            # 4: constant heat flux boundary condition
 SOLVE_FOR_VOLATILES = "1"            # track evolution of volatiles in interior/atmosphere reservoirs
-H2O_poststep_change = "0.50"         # fractional change in melt phase H2O concentration that triggers event
-CO2_poststep_change = "0.50"         # as above for CO2, also a 5% change to trigger an event
+H2O_poststep_change = "1.00"         # fractional change in melt phase H2O concentration that triggers event
+CO2_poststep_change = "1.00"         # as above for CO2 to trigger an event
 nstepsmacro         = "50"           # number of timesteps
 dtmacro             = "20000"        # delta time per macrostep to advance by, in years
 heat_flux           = "1.0E4"        # prescribed start surface heat flux (e.g., 10^4 W/m^2)
 
 # Restart flag
-start_condition     = "1"            # 1: Start from beginning, 2: Restart from file
+start_condition     = "2"            # 1: Start from beginning, 2: Restart from file "ic_filename"
 
 # Total runtime
 time_current = 0
-time_goal     = 1000000
+time_target  = 1000000
 
-# Start SPIDER to generate first output file
-call_sequence = [ "spider", "-options_file", "bu_input.opts", "-initial_condition", "1", "-ic_filename", "output/"+ic_filename, "-SURFACE_BC", SURFACE_BC, "-surface_bc_value", heat_flux, "-SOLVE_FOR_VOLATILES", SOLVE_FOR_VOLATILES, "-activate_rollback", "-activate_poststep", "-H2O_poststep_change", H2O_poststep_change, "-CO2_poststep_change", CO2_poststep_change, "-nstepsmacro", "0", "-dtmacro", "1" ]
-print(call_sequence)
-subprocess.call(call_sequence)
+# Initialize SPIDER to generate first output file
+if start_condition == "1":
 
-# Set restart flag
-start_condition     = "2"
+    # SPIDER initialization call sequence
+    call_sequence = [ "spider", "-options_file", "bu_input.opts", "-initial_condition", "1", "-ic_filename", "output/"+ic_filename, "-SURFACE_BC", SURFACE_BC, "-surface_bc_value", heat_flux, "-SOLVE_FOR_VOLATILES", SOLVE_FOR_VOLATILES, "-activate_rollback", "-activate_poststep", "-H2O_poststep_change", H2O_poststep_change, "-CO2_poststep_change", CO2_poststep_change, "-nstepsmacro", "0", "-dtmacro", "1" ]
 
-while time_current < time_goal:
+    # Runtime info
+    print("SPIDER run flags:", call_sequence)
+
+    # Call SPIDER
+    subprocess.call(call_sequence)
+
+    # Reset restart flag
+    start_condition     = "2"
+
+# Ping-poing between SPIDER and SOCRATES until target time is reached
+while time_current < time_target:
 
     # SPIDER restart call sequence
     call_sequence = [ "spider", "-options_file", "bu_input.opts", "-initial_condition", start_condition, "-ic_filename", "output/"+ic_filename, "-SURFACE_BC", SURFACE_BC, "-surface_bc_value", heat_flux, "-SOLVE_FOR_VOLATILES", SOLVE_FOR_VOLATILES, "-activate_rollback", "-activate_poststep", "-H2O_poststep_change", H2O_poststep_change, "-CO2_poststep_change", CO2_poststep_change, "-nstepsmacro", nstepsmacro, "-dtmacro", dtmacro ]
-    print(call_sequence)
+
+    # Runtime info
+    print("SPIDER run flags:", call_sequence)
 
     # Restart SPIDER
     subprocess.call(call_sequence)
@@ -70,12 +80,28 @@ while time_current < time_goal:
     ic_filename = natsorted([os.path.basename(x) for x in glob.glob("./output/*.json")])[-1]
 
     # print current values
-    print("time – T_surf – h2o – co2 – heat_flux – last_file")
-    print(time_current, surfaceT_current, h2o_current, co2_current, heat_flux, ic_filename)
+    # print("time – T_surf – h2o – co2 – heat_flux – last_file")
+    # print(time_current, surfaceT_current, h2o_current, co2_current, heat_flux, ic_filename)
+    print("Time [Myr]:", str(float(time_current)/1e6))
+    print ("T_surf [K]:", surfaceT_current)
+    print ("H2O [kg]:", h2o_current)
+    print ("CO2 [kg]:", co2_current)
+    print ("OLR [W]:", heat_flux)
+    print ("Last file name:", ic_filename)
 
     # Set to str for subprocess
     heat_flux = str(heat_flux)
 
-    # Reset number of computing steps to reach time_goal
-    dtime       = time_goal - time_current
+    # Reset number of computing steps to reach time_target
+    dtime       = time_target - time_current
     nstepsmacro = str( math.ceil( dtime / float(dtmacro) ) )
+
+# Print final statement
+print("########## Target time reached, final values:")
+print("Time [Myr]:", str(float(time_current)/1e6))
+print ("T_surf [K]:", surfaceT_current)
+print ("H2O [kg]:", h2o_current)
+print ("CO2 [kg]:", co2_current)
+print ("OLR [W]:", heat_flux)
+print ("Last file name:", ic_filename)
+print("########## / FIN ")
