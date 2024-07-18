93ff5ed29224cca8b219fd2e426dc9ba64ec6aed
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2024-07-18 15:15:28 +0200

    Small fix.

--- proteus.py
+++ proteus.py
@@ -160,9 +160,9 @@ def main():
 
     # Set up spectral files
     if os.environ.get('FWL_DATA') == None:
-        raise Exception("The FWL_DATA environment variable where spectral
-                        and evolution tracks data will be downloaded needs to be set up!
-                        Did you source PROTEUS.env?")
+        raise Exception("The FWL_DATA environment variable where spectral"
+                        "and evolution tracks data will be downloaded needs to be set up!"
+                        "Did you source PROTEUS.env?")
     else:
         fwl_data_dir = os.environ.get('FWL_DATA')
 
