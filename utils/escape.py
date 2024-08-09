5ff6428cf0000d2286a7f908faaded3f0477113f
Emma Postolec <e.n.postolec@rug.nl>
2024-08-09 11:37:20 +0200

    Try to make Fxuv_star_SI_full and age_star_mors global variables : failed for now

--- utils/escape.py
+++ utils/escape.py
@@ -104,11 +104,12 @@ def RunZEPHYRUS(hf_row, dt, M_star,Omega_star,tidal_contribution, semi_major_axi
 
     # Get the age of the star at time t to compute XUV flux at that time 
     age_star        = hf_row["age_star"]                                                # [years] 
-    if (Fxuv_star_SI_full == None):
-        star = mors.Star(Mstar=M_star, Age=age_star/1e6, Omega=Omega_star) 
-        age_star_mors   = star.Tracks['Age']
+
+    if (Fxuv_star_SI_full is None):
+        star                = mors.Star(Mstar=M_star, Age=age_star/1e6, Omega=Omega_star) 
+        age_star_mors       = star.Tracks['Age']
         Fxuv_star_SI_full   = ((star.Tracks['Lx'] + star.Tracks['Leuv']) / (4 * np.pi * (semi_major_axis * 1e2)**2)) * ergcm2stoWm2
-    
+
     # Interpolating the XUV flux at the age of the star
     Fxuv_star_SI        = np.interp(age_star, age_star_mors * 1e6, Fxuv_star_SI_full)                                            # Interpolate to get Fxuv at age_star
     log.info(f"Interpolated Fxuv_star_SI at age_star = {age_star} years is {Fxuv_star_SI}")
