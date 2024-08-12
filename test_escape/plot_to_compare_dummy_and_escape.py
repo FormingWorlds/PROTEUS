a05ae24ba74f7d3d0d3f792f4da5874273c3c11b
Emma Postolec <e.n.postolec@rug.nl>
2024-08-12 09:54:10 +0200

    Comparison escape : Zephyrus vs dummy plot (dummy simulation failed)

--- test_escape/plot_to_compare_dummy_and_escape.py
+++ test_escape/plot_to_compare_dummy_and_escape.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 path_to_csv = '/Users/emmapostolec/Documents/PHD/SCIENCE/CODES/PROTEUS/output/earth_demo/'
 
 # Load .csv files
-escape_csv            = 'runtime_helpfile.csv'
-df_escape             = pd.read_csv(path_to_csv+escape_csv, delimiter='\t')   # Zephyrus escape
+escape_csv            = 'runtime_helpfile_zephyrus.csv'
+df_escape             = pd.read_csv(escape_csv, delimiter='\t')   # Zephyrus escape
 time_column_e         = df_escape['Time']
 escape_rate_column_e  = df_escape['esc_rate_total']
 
@@ -19,7 +19,7 @@ plt.figure(figsize=(10, 6))
 plt.loglog(time_column_d, escape_rate_column_d, label='Dummy escape', color='orange')
 plt.loglog(time_column_e, escape_rate_column_e, label='EL escape (Zephyrus)', color='steelblue')
 plt.xlabel('Time [years]')
-plt.ylabel('Total Escape Rate [kg s^-1]')
+plt.ylabel(r'Total Escape Rate [kg $s^{-1}$]')
 plt.legend()
 plt.grid(alpha=0.5)
 plt.savefig('comparison_dummy_zephyrus.png', dpi=180)
