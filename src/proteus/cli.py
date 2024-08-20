a0de2f48f0d6af63d599d2d902f74180731cf23c
Stef Smeets <s.smeets@esciencecenter.nl>
2024-08-20 15:01:45 +0200

    Move code away from start_proteus.py to Proteus class

--- src/proteus/cli.py
+++ src/proteus/cli.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 import click
 
 
@@ -12,13 +14,30 @@ def cli():
 def version():
     """Print version and exit"""
     from . import __version__
+
     print(__version__)
 
 
 @click.command()
-def start():
+@click.option(
+    '-c',
+    '--config',
+    'config_path',
+    type=click.Path(exists=True, dir_okay=False, path_type=Path, resolve_path=True),
+    help='Path to config file',
+)
+@click.option(
+    '-r',
+    '--resume',
+    is_flag=True,
+    default=False,
+    help='Resume simulation from disk',
+)
+def start(config_path: Path, resume: bool):
     """Start proteus run"""
-    pass
+    from .proteus import Proteus
+    runner = Proteus(config_path=config_path)
+    runner.start(resume=resume)
 
 
 cli.add_command(start)
