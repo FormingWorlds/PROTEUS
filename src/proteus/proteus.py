92214538db4cb955cfe75c997a02be2856a80cf9
Stef Smeets <s.smeets@esciencecenter.nl>
2024-08-22 10:41:52 +0200

    Fix toml 3.10 compatibility

--- src/proteus/proteus.py
+++ src/proteus/proteus.py
@@ -60,16 +60,17 @@ from proteus.utils.surface_gases import (
 )
 
 if sys.version_info < (3, 11):
-    import toml as tomllib
+    import toml
+
+    read_config = toml.load
 else:
     import tomllib
 
-
-def read_config(path: Path | str) -> dict:
-    """Read config file from path"""
-    with open(path, 'rb') as f:
-        config = tomllib.load(f)
-    return config
+    def read_config(path: Path | str) -> dict:
+        """Read config file from path"""
+        with open(path, 'rb') as f:
+            config = tomllib.load(f)
+        return config
 
 
 class Proteus:
