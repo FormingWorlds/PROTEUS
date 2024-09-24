45350b1bcf83501fd1ba591b85cd8de4cd5ec6cf
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2024-09-24 13:35:06 +0200

    Trigger the integration test by the CI.

--- tests/integration/test_dummy_integration.py
+++ tests/integration/test_dummy_integration.py
@@ -10,11 +10,6 @@ from pandas.testing import assert_frame_equal
 from proteus import Proteus
 from proteus.utils.coupler import ReadHelpfileFromCSV
 
-if os.getenv('CI'):
-    # https://github.com/FormingWorlds/PROTEUS/pull/149
-    pytest.skip(reason='No way of currently testing this on the CI.', allow_module_level=True)
-
-
 def test_dummy_run():
     config_path = PROTEUS_ROOT / 'input' / 'dummy.toml'
     ref_output = PROTEUS_ROOT / 'tests' / 'data' / 'integration' / 'dummy' #/ 'runtime_helpfile.csv'
