bcaeef0f0d2bb7177f5ffeb83494036743b5d15c
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2024-09-26 11:06:00 +0200

    Rewrite poltting test as parametrized.

--- tests/integration/test_dummy_integration.py
+++ tests/integration/test_dummy_integration.py
@@ -20,7 +20,7 @@ IMAGE_LIST = (
         "plot_sflux.png",
         )
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def test_dummy_run():
     config_path = PROTEUS_ROOT / 'input' / 'dummy.toml'
     ref_output = PROTEUS_ROOT / 'tests' / 'data' / 'integration' / 'dummy'
@@ -37,24 +37,24 @@ def test_dummy_run():
     assert_frame_equal(hf_all, hf_all_ref, rtol=1e-4)
 
 @pytest.mark.xfail
-def test_plot_dummy_integration(test_dummy_run):
+@pytest.mark.parametrize("image", IMAGE_LIST)
+def test_plot_dummy_integration(test_dummy_run, image):
 
     out_dir = PROTEUS_ROOT / 'output' / 'dummy'
     ref_dir = PROTEUS_ROOT / 'tests' / 'data' / 'integration' / 'dummy'
 
-    for image in IMAGE_LIST:
-        out_img = out_dir / image
-        ref_img = ref_dir / image
-        tolerance = 3
+    out_img = out_dir / image
+    ref_img = ref_dir / image
+    tolerance = 3
 
-        # Resize images if needed
-        out_tmp, ref_tmp = resize_to_match(out_img, ref_img)
+    # Resize images if needed
+    out_tmp, ref_tmp = resize_to_match(out_img, ref_img)
 
-        # Save resized images to temporary paths
-        out_tmp.save('test.png')
-        ref_tmp.save('ref.png')
+    # Save resized images to temporary paths
+    out_tmp.save('test.png')
+    ref_tmp.save('ref.png')
 
-        # Use compare_images to compare the resized files
-        result = compare_images('test.png', 'ref.png', tol=tolerance)
+    # Use compare_images to compare the resized files
+    result = compare_images('test.png', 'ref.png', tol=tolerance)
 
-        assert result is None, f"The two PNG files {image} differ more than the allowed tolerance: {result}"
+    assert result is None, f"The two PNG files {image} differ more than the allowed tolerance: {result}"
