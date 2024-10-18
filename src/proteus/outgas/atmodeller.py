141b927c08513f1ec830bb362092db602de0e7cc
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-10-18 10:49:26 +0100

    Linting

--- src/proteus/outgas/atmodeller.py
+++ src/proteus/outgas/atmodeller.py
@@ -1,2 +1,10 @@
 # Wrapper functions for atmodeller
+from __future__ import annotations
 
+import logging
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
+
+log = logging.getLogger("fwl."+__name__)
