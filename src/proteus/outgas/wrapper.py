7974e56add6981d43d0a13f9d753fcea0fe74bf5
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-10-18 09:15:10 +0100

    Further stubs

--- src/proteus/outgas/wrapper.py
+++ src/proteus/outgas/wrapper.py
@@ -1,10 +1,19 @@
-# Function and classes used to run CALLIOPE
+# Generic interior wrapper
 from __future__ import annotations
 
 import logging
 from typing import TYPE_CHECKING
 
+from proteus.utils.helper import PrintHalfSeparator
+
 if TYPE_CHECKING:
     from proteus.config import Config
 
 log = logging.getLogger("fwl."+__name__)
+
+def run_outgas():
+    '''
+    Run volatile outgassing model
+    '''
+
+    PrintHalfSeparator()
