75069d23ce1bae380056376ff2e691f941221dfa
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-10-25 14:59:36 +0100

    Linting

--- tests/integration/test_integration_dummy.py
+++ tests/integration/test_integration_dummy.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+import filecmp
 from pathlib import Path
 
 import pytest
-import filecmp
 from helpers import PROTEUS_ROOT, resize_to_match
 from matplotlib.testing.compare import compare_images
 from pandas.testing import assert_frame_equal
