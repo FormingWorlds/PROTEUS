1e8cfc9570eb654ca8207c1ee4f0a3b64306f4a8
Emma Postolec <e.n.postolec@rug.nl>
2024-10-28 13:44:25 +0100

    Update min and max value of Pxuv parameter

--- src/proteus/config/_escape.py
+++ src/proteus/config/_escape.py
@@ -40,10 +40,9 @@ class Zephyrus:
     tidal: bool
         Tidal contribution enabled
     """
-    Pxuv: float = field(default=1e-2, validator=[ge(1e-5), le(1e5)])
+    Pxuv: float = field(default=1e-2, validator=[ge(1e-10), le(1e1)])
     efficiency: float = field(default=1.0,validator=(ge(0.0), le(1.0)))
     tidal: bool = field(default=False)
-
 
 
 @define
