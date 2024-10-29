6f9fc26f41d281841bc8a06c02cab10449d6be8d
Stef Smeets <s.smeets@esciencecenter.nl>
2024-10-29 10:24:44 +0100

    Add test for calliope method

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,6 +8,7 @@ from helpers import PROTEUS_ROOT
 from proteus import Proteus
 from proteus.config import Config, read_config_object
 from proteus.config._converters import none_if_none
+from proteus.config._outgas import Calliope
 
 PATHS = chain(
     (PROTEUS_ROOT / 'input').glob('*.toml'),
@@ -50,3 +51,15 @@ def test_proteus_init(path):
     runner = Proteus(config_path=path)
 
     assert isinstance(runner.config, Config)
+
+
+def test_calliope_converter():
+    conf = Calliope(
+        T_floor=0.1,
+        include_CO=False,
+        include_H2=True,
+    )
+    assert not conf.is_included('CO')
+    assert conf.is_included('H2')
+    with pytest.raises(AttributeError):
+        conf.is_included('fails')
