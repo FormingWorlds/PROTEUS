875218b2932379626f8b3e74dadf9845cd3454b9
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-11-03 10:25:42 +0000

    Compare atmosphere output arrays

--- tests/integration/test_integration_physical.py
+++ tests/integration/test_integration_physical.py
@@ -6,11 +6,11 @@ from pathlib import Path
 import pytest
 from helpers import PROTEUS_ROOT
 from pandas.testing import assert_frame_equal
+from np.testing import assert_array_equal
 
 from proteus import Proteus
 from proteus.utils.coupler import ReadHelpfileFromCSV
-from proteus.atmos_clim.common import read_atmosphere_data
-from proteus.interior.wrapper import read_interior_data
+from proteus.atmos_clim.common import read_ncdf_profile
 from proteus.utils.plot import sample_output
 
 out_dir = PROTEUS_ROOT / 'output' / 'physical'
@@ -31,34 +31,23 @@ def test_physical_run(physical_run):
     # Check helpfile
     assert_frame_equal(hf_all, hf_all_ref, rtol=5e-3)
 
-    # Check statusfiles
-    assert filecmp.cmp(out_dir / 'status', ref_dir / 'status', shallow=False)
-
     # Check stellar spectra
     assert filecmp.cmp(out_dir / 'data' / '0.sflux', ref_dir / '0.sflux', shallow=False)
 
 def test_physical_atmosphere(physical_run):
     # Get output times
-    times,_ = sample_output(physical_run, extension="_atm.nc")
+    # times,_ = sample_output(physical_run, extension="_atm.nc")
 
     # Load atmosphere output
-    atm_data = read_atmosphere_data(out_dir,times)
+    atm_out = read_ncdf_profile(out_dir / 'data' / '2002_atm.nc')
 
     # Compare to config
-    for i,atm in enumerate(atm_data):
-        assert len(atm["t"]) == physical_run.config.atmos_clim.janus.num_levels*2+1
+    assert len(atm_out["t"]) == physical_run.config.atmos_clim.janus.num_levels*2+1
+
+    # Load atmosphere reference
+    atm_ref = read_ncdf_profile(ref_dir / '2002_atm.nc')
 
     # Compare to expected
-
-
-
-def test_physical_interior(physical_run):
-    # Get output times
-    times,_ = sample_output(physical_run, extension="_atm.nc")
-
-    # Check Aragog output
-    int_data = read_interior_data(out_dir,times)
-
-    # Compare to config
-    for i,atm in enumerate(int_data):
-        assert len(atm["t"]) == physical_run.config.atmos_clim.janus.num_levels*2+1
+    # Cannot simply compare the files as black-boxes, because they contain date information
+    for field in ["tmp","p","fl_U_LW"]:
+        assert_allclose(atm_out[field], atm_ref[field], rtol=5e-3)
