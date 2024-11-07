620db0f5f8ec5f05ae128f3c62855b12fefd6748
Stef Smeets <s.smeets@esciencecenter.nl>
2024-11-07 16:35:00 +0100

    Remove config compatibility code

--- tests/test_config.py
+++ tests/test_config.py
@@ -33,20 +33,6 @@ def test_none_if_none():
         assert none_if_none(val) is not None
 
 
-def test_dict_interface(cfg):
-    # test dict interface for OPTIONS compatibility
-    assert cfg['star_model'] == 'baraffe'
-
-    with pytest.raises(ValueError):
-        cfg['Phi_global']
-
-    with pytest.raises(ValueError):
-        cfg['shallow_ocean_layer']
-
-    with pytest.raises(TypeError):
-        cfg['star_model'] = 'spada'
-
-
 @pytest.mark.parametrize('path', PATHS)
 def test_proteus_init(path):
     runner = Proteus(config_path=path)
