96549e1e482e6a418993de764fac2592329375f4
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-11-12 12:13:01 +0000

    Stub for orbit

---
+++ src/proteus/orbit/wrapper.py
@@ -0,0 +1,9 @@
+# Generic orbital dynamics stuff
+from __future__ import annotations
+
+import logging
+
+from proteus.utils.constants import AU, R_sun, const_sigma
+
+log = logging.getLogger("fwl."+__name__)
+
