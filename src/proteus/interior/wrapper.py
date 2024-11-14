67cb4a7ec721e46496d8fad41ad815fec53fe2f8
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-11-14 15:21:46 +0000

    K2-18b vaguely working

--- src/proteus/interior/wrapper.py
+++ src/proteus/interior/wrapper.py
@@ -60,7 +60,7 @@ def determine_interior_radius(dirs:dict, config:Config, hf_all:pd.DataFrame, hf_
             update_gravity(hf_row)
 
         res = hf_row["M_int"] - M_target
-        log.debug("    found M = %.5e kg"%hf_row["M_int"])
+        log.debug("    found M = %.5e kg , resid = %.3e kg"%(hf_row["M_int"], res))
 
         return res
 
