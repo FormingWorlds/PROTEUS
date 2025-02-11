8fdd6d3b1ea209ed7d8888dcdd0bf32adf15d1eb
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-02-11 12:05:36 +0100

    Only compare overlapping columns in test dataframe. This allows us to test the model easily even if new columns have been added.

--- tests/helpers/helpers.py
+++ tests/helpers/helpers.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from PIL import Image
 
 PROTEUS_ROOT = Path(__file__).parents[2]
+NEGLECT = ["CH4_mol_atm", "CH4_mol_total", "CH4_kg_atm", "CH4_kg_total", "CH4_bar", "runtime"]
 
 def resize_to_match(image1_path, image2_path):
     img1 = Image.open(image1_path)
@@ -15,3 +16,20 @@ def resize_to_match(image1_path, image2_path):
         img2 = img2.resize(img1.size)
 
     return img1, img2
+
+def df_intersect(df1, df2):
+    """
+    Filter two dataframes to only the columns which they have in common.
+    """
+
+    # Intersection of columns
+    inter = set(df1.columns).intersection(set(df2.columns))
+    inter = list(inter)
+
+    # Filter
+    out1 = df1.loc[:, df1.columns.isin(inter)]
+    out2 = df2.loc[:, df2.columns.isin(inter)]
+
+    # Return
+    return out1, out2
+
