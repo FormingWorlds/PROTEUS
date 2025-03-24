63225b0f5446f1927ffe01a4e0e81f0a479110c7
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-03-24 13:10:34 +0000

    More optional stuff

--- src/proteus/config/_params.py
+++ src/proteus/config/_params.py
@@ -13,27 +13,6 @@ def max_bigger_than_min(instance, attribute, value):
 
 
 @define
-class Params:
-    """Parameters for code execution, output files, time-stepping, convergence.
-
-    Attributes
-    ----------
-    out: OutputParams
-        Parameters for data / logging output.
-    dt: TimeStepParams
-        Parameters for time-stepping.
-    stop: StopParams
-        Parameters for stopping criteria.
-    """
-    out: OutputParams
-    dt: TimeStepParams
-    stop: StopParams
-
-    resume: bool = field(default = False)
-    offline: bool = field(default = False)
-
-
-@define
 class OutputParams:
     """Parameters for output files and logging
 
@@ -51,7 +30,8 @@ class OutputParams:
         Write CSV frequency. 0: wait until completion. n: every n iterations.
     """
     path: str
-    logging: str    = field(validator=in_(('INFO', 'DEBUG', 'ERROR', 'WARNING')))
+    logging: str    = field(default='INFO',
+                            validator=in_(('INFO', 'DEBUG', 'ERROR', 'WARNING')))
     plot_mod: int   = field(default=1,      validator=ge(0))
     plot_fmt: str   = field(default='png',  validator=in_(('pdf', 'png')))
     write_mod: int  = field(default=1,      validator=ge(0))
@@ -79,8 +59,8 @@ class DtAdaptive:
     rtol: float
         Relative tolerance on time-step size [dimensionless].
     """
-    atol: float = field(validator=gt(0))
-    rtol: float = field(validator=gt(0))
+    atol: float = field(default=0.02, validator=gt(0))
+    rtol: float = field(default=0.10, validator=gt(0))
 
 @define
 class TimeStepParams:
@@ -107,8 +87,8 @@ class TimeStepParams:
     """
 
 
-    starspec: float = field(validator=ge(0))
-    starinst: float = field(validator=ge(0))
+    starspec: float = field(default=3e6, validator=ge(0))
+    starinst: float = field(default=1e3, validator=ge(0))
 
     method: str = field(default='adaptive',
                         validator=in_(('proportional', 'adaptive', 'maximum')))
@@ -120,6 +100,89 @@ class TimeStepParams:
     maximum: float = field(default=1e7, validator=gt(0))
     initial: float = field(default=1e3, validator=gt(0))
 
+
+@define
+class StopIters:
+    """Parameters for iteration number criteria.
+
+    Attributes
+    ----------
+    enabled: bool
+        Enable criteria if True
+    minimum: int
+        Minimum number of iterations.
+    maximum: int
+        Maximum number of iterations.
+    """
+    enabled: bool = field(default=True)
+    minimum: int  = field(default=5, validator=ge(0))
+    maximum: int  = field(default=9000, validator=max_bigger_than_min)
+
+
+@define
+class StopTime:
+    """Parameters for maximum time criteria.
+
+    Attributes
+    ----------
+    enabled: bool
+        Enable criteria if True
+    minimum: float
+        Model will absolutely not terminate until at least this time is reached [yr].
+    maximum: float
+        Model will terminate when this time is reached [yr].
+    """
+    enabled: bool  = field(default=True)
+    maximum: float = field(default=5e9, validator=max_bigger_than_min)
+    minimum: float = field(default=1e3, validator=ge(0))
+
+
+@define
+class StopSolid:
+    """Parameters for solidification criteria.
+
+    Attributes
+    ----------
+    enabled: bool
+        Enable criteria if True.
+    phi_crit: float
+        Model will terminate when global melt fraction is less than this value [dimensionless].
+    """
+    phi_crit: float = field(default=0.005, validator=(gt(0), lt(1)))
+    enabled: bool   = field(default=True)
+
+
+@define
+class StopRadeqm:
+    """Parameters for radiative equilibrium stopping criteria.
+
+    Attributes
+    ----------
+    enabled: bool
+        Enable criteria if True
+    atol: float
+        Absolute tolerance on energy balance [W m-2].
+    rtol: float
+        Relative tolerance on energy balance.
+    """
+    enabled: bool   = field(default=True)
+    atol: float     = field(default=0.1,  validator=gt(0))
+    rtol: float     = field(default=1e-3, validator=ge(0))
+
+
+@define
+class StopEscape:
+    """Parameters for escape stopping criteria.
+
+    Attributes
+    ----------
+    enabled: bool
+        Enable criteria if True
+    p_stop: float
+        Model will terminate when surface pressure is less than this value [bar].
+    """
+    enabled: bool   = field(default=True)
+    p_stop:float    = field(default=1, validator=(gt(0),lt(1e6)))
 
 
 @define
@@ -141,93 +204,30 @@ class StopParams:
     escape: StopEscape
         Parameters for escape criteria.
     """
-    iters: StopIters
-    time: StopTime
-    solid: StopSolid
-    radeqm: StopRadeqm
-    escape: StopEscape
+    iters: StopIters   = field(factory=StopIters)
+    time: StopTime     = field(factory=StopTime)
+    solid: StopSolid   = field(factory=StopSolid)
+    radeqm: StopRadeqm = field(factory=StopRadeqm)
+    escape: StopEscape = field(factory=StopEscape)
 
     strict: bool        = field(default=False)
 
 @define
-class StopIters:
-    """Parameters for iteration number criteria.
+class Params:
+    """Parameters for code execution, output files, time-stepping, convergence.
 
     Attributes
     ----------
-    enabled: bool
-        Enable criteria if True
-    minimum: int
-        Minimum number of iterations.
-    maximum: int
-        Maximum number of iterations.
+    out: OutputParams
+        Parameters for data / logging output.
+    dt: TimeStepParams
+        Parameters for time-stepping.
+    stop: StopParams
+        Parameters for stopping criteria.
     """
-    enabled: bool
-    minimum: int  = field(default=5, validator=ge(0))
-    maximum: int  = field(default=9000, validator=max_bigger_than_min)
+    out: OutputParams  = field(factory=OutputParams)
+    dt: TimeStepParams = field(factory=TimeStepParams)
+    stop: StopParams   = field(factory=StopParams)
 
-
-@define
-class StopTime:
-    """Parameters for maximum time criteria.
-
-    Attributes
-    ----------
-    enabled: bool
-        Enable criteria if True
-    minimum: float
-        Model will absolutely not terminate until at least this time is reached [yr].
-    maximum: float
-        Model will terminate when this time is reached [yr].
-    """
-    enabled: bool
-    maximum: float = field(validator=max_bigger_than_min)
-    minimum: float = field(default=1e3, validator=ge(0))
-
-
-@define
-class StopSolid:
-    """Parameters for solidification criteria.
-
-    Attributes
-    ----------
-    enabled: bool
-        Enable criteria if True.
-    phi_crit: float
-        Model will terminate when global melt fraction is less than this value [dimensionless].
-    """
-    enabled: bool
-    phi_crit: float = field(validator=(gt(0), lt(1)))
-
-
-@define
-class StopRadeqm:
-    """Parameters for radiative equilibrium stopping criteria.
-
-    Attributes
-    ----------
-    enabled: bool
-        Enable criteria if True
-    atol: float
-        Absolute tolerance on energy balance [W m-2].
-    rtol: float
-        Relative tolerance on energy balance.
-    """
-    enabled: bool
-    atol: float = field(validator=gt(0))
-    rtol: float = field(validator=ge(0))
-
-
-@define
-class StopEscape:
-    """Parameters for escape stopping criteria.
-
-    Attributes
-    ----------
-    enabled: bool
-        Enable criteria if True
-    p_stop: float
-        Model will terminate when surface pressure is less than this value [bar].
-    """
-    enabled: bool
-    p_stop:float = field(validator=(gt(0),lt(1e6)))
+    resume: bool       = field(default = False)
+    offline: bool      = field(default = False)
