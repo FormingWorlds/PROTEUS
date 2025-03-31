f6df69874c4e16eacdd7ff63f17cbe637eecd470
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-03-31 12:55:15 +0100

    Code style 2

--- src/proteus/outgas/calliope.py
+++ src/proteus/outgas/calliope.py
@@ -13,6 +13,7 @@ from calliope.solve import (
     get_target_from_params,
     get_target_from_pressures,
 )
+
 from proteus.utils.constants import element_list, vol_list
 from proteus.utils.helper import UpdateStatusfile
 
