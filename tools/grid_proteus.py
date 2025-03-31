5e5b042dc0c7676fda2daf6c04d22fa919c791e5
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-03-31 15:24:00 +0100

    L98 configs

--- tools/grid_proteus.py
+++ tools/grid_proteus.py
@@ -477,7 +477,7 @@ if __name__=='__main__':
     # -----
     # Start PROTEUS processes
     # -----
-    pg.run(90, test_run=False)
+    pg.run(105, test_run=False)
 
     # When this script ends, it means that all processes ARE complete or they
     # have been killed or crashed.
