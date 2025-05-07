0fda6ca5be5556f2ae9f095c80e069351d0ff2b9
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2025-05-07 16:48:17 +0200

    Colour science

--- src/proteus/plot/cpl_visual.py
+++ src/proteus/plot/cpl_visual.py
@@ -6,7 +6,8 @@ import os
 from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+from matplotlib import patches
+from matplotlib import ticker
 import numpy as np
 
 from proteus.atmos_clim.common import read_ncdf_profile
@@ -21,7 +22,7 @@ log = logging.getLogger("fwl."+__name__)
 
 
 def plot_visual(hf_all:pd.DataFrame, output_dir:str,
-                    idx=-1, osamp=2, plot_format="pdf"):
+                    idx=-1, osamp=3, view=12.5, plot_format="pdf"):
 
     log.info("Plot visual")
 
@@ -32,7 +33,7 @@ def plot_visual(hf_all:pd.DataFrame, output_dir:str,
 
     # Set viewing distance
     R_int = float(hf_all["R_int"].iloc[idx])
-    obs = R_int * 12
+    obs = R_int * view
 
     # Get output NetCDF file
     time  = hf_all["Time"].iloc[idx]
@@ -71,33 +72,34 @@ def plot_visual(hf_all:pd.DataFrame, output_dir:str,
 
     # radii
     r_arr = ds["rl"] / obs
-    r_min, r_max = np.amin(r_arr), np.amax(r_arr)
+    r_min = np.amin(r_arr)
     r_lim = 0.2
     n_lev = len(r_arr)
 
     # pressures
-    p_arr = ds["pl"] / 1e5 # bar
-    p_min, p_max = np.amin(p_arr), np.amax(p_arr)
+    p_arr = ds["pl"]
+    p_max = np.amax(p_arr)
 
     # plot surface of planet
-    col = colsys.spec_to_rgb(interp_spec(wl, lw[-1,:]+sw[-1,:]))
-    srf = patches.Circle((0,0), radius=r_min, fc=col, zorder=20)
+    fl_srf = lw[-1,:]+sw[-1,:]
+    col = colsys.spec_to_rgb(interp_spec(wl, fl_srf))
+    srf = patches.Circle((0,0), radius=r_min, fc=col, zorder=8)
+    srf = patches.Circle((0,0), radius=r_min, fc=col, zorder=n_lev+1, alpha=0.2)
     ax.add_patch(srf)
 
     # level opacities
-    gamma = 0.05
+    gamma = 0.12
     a_arr = []
-    for i in range(n_lev-1):
-        alp = abs(p_arr[i] - p_arr[i+1]) / p_max
+    for i,p in enumerate(p_arr):
+        alp = p / p_max
         a_arr.append(alp**gamma)
-    a_arr /= sum(a_arr)*osamp
-    a_arr *= 0.9
+    a_arr /= sum(a_arr)
+    a_arr *= 0.99
 
-    # plot levels
+    # plot outer levels
     for i in range(n_lev-2,-1,-1):
-
-        sw_lev = sw[i,:] - sw[i+1,:]
-        lw_lev = lw[i,:] - lw[i+1,:]
+        sw_lev = sw[i+1,:] - sw[i,:]
+        lw_lev = lw[i+1,:] - lw[i,:]
 
         rad_c = r_arr[i]
         rad_l = r_arr[i+1]
@@ -111,11 +113,11 @@ def plot_visual(hf_all:pd.DataFrame, output_dir:str,
             ax.add_patch(cir)
 
     # annotate planet
-    ax.text(0,0.5*R_int/obs, "Planet", color='white', fontsize=12, ha='center', va='center', zorder=999)
+    ax.text(0,0.2*R_int/obs, r"T$_\text{s}=$%.0f K"%ds["tmpl"][-1], color='white', fontsize=11, ha='center', va='bottom', zorder=999)
 
     # annotate time and distance
     ann = r"Viewing from %.1f R$_\oplus$"%(obs/R_earth) + " at %6.1f Myr"%(time/1e6)
-    ax.text(0.01, 0.99, ann, color='white', fontsize=12, zorder=999,
+    ax.text(0.01, 0.99, ann, color='white', fontsize=11, zorder=999,
                 transform=ax.transAxes, ha='left', va='top')
 
     # plot star
@@ -124,7 +126,7 @@ def plot_visual(hf_all:pd.DataFrame, output_dir:str,
     x_star = r_lim * 0.75
     cir = patches.Circle((x_star,x_star), radius=r_star, fc=col, zorder=2,)
     ax.add_patch(cir)
-    ax.text(x_star, x_star, "Star", color='white', fontsize=12,
+    ax.text(x_star, x_star, "Star", color='white', fontsize=11,
                 ha='center', va='center', zorder=999)
 
     # scale bar
@@ -145,16 +147,32 @@ def plot_visual(hf_all:pd.DataFrame, output_dir:str,
     ax.get_yaxis().set_visible(False)
 
     # inset spectrum
-    axr = ax.inset_axes((0.08, 0.08, 0.32,0.25))
-    axr.set_facecolor('k')
-    imax = np.argmin(np.abs(wl-2500))
-    axr.plot(wl[:imax], lw[0,:imax]+sw[0,:imax], color='w', lw=1.3)
+    axr = ax.inset_axes((0.07, 0.04, 0.39,0.21))
+    axr.set_alpha(0.0)
+    axr.set_facecolor((0,0,0,0))
+    imax = np.argmin(np.abs(wl-6e3))
+    fl  = lw[0,:]+sw[0,:]
+    wd  = (ds["bandmax"]-ds["bandmin"]) * 1e9
+    fl  = fl / wd
+    if np.amax(fl) < 1:
+        fl *= 1e3
+        un = "mW"
+    else:
+        un = "W"
+    axr.bar(wl[:imax], fl[:imax], width=wd[:imax], color='w', lw=1.3)
     axr.spines[['bottom','left']].set_color('w')
     axr.spines[['right', 'top']].set_visible(False)
     axr.tick_params(axis='both', colors='w', labelsize=8)
-    axr.set_xlabel("Wavelength [nm]", color='w', fontsize=8)
-    axr.set_ylabel("Emission", color='w', fontsize=8)
+    axr.set_xlabel(r"$/$nm", color='w', fontsize=8)
+    axr.xaxis.set_label_coords(1.12, -0.08)
+    axr.set_ylabel(r"%s/m$^2$/nm"%un, color='w', fontsize=8, rotation=0)
+    axr.yaxis.set_label_coords(0.01, 1.02)
+
+    axr.set_xlim(left=0)
+    axr.xaxis.set_major_locator(ticker.MultipleLocator(1000))
+    axr.xaxis.set_minor_locator(ticker.MultipleLocator(200))
     axr.set_ylim(bottom=0)
+    axr.yaxis.set_major_formatter(ticker.FormatStrFormatter("%g"))
 
     plt.close()
     plt.ioff()
