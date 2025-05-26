Fixed radius fraction precision

src/proteus/interior/zalmoxis.py
@@ -364,8 +364,8 @@ def zalmoxis_solver(config:Config, outdir:str, hf_row:dict):
     log.info(f"Pressure at the center: {pressure[0]:.2e} Pa")
     log.info(f"Average density: {average_density:.2e} kg/m^3")
     log.info(f"Core-mantle boundary mass fraction: {mass_enclosed[cmb_index] / mass_enclosed[-1]:.3f}")
-    log.info(f"Core radius fraction: {cmb_radius / planet_radius:.3f}")
-    log.info(f"Inner mantle radius fraction: {radii[np.argmax(mass_enclosed >= inner_mantle_mass)] / planet_radius:.3f}")
+    log.info(f"Core radius fraction: {cmb_radius / planet_radius:.4f}")
+    log.info(f"Inner mantle radius fraction: {radii[np.argmax(mass_enclosed >= inner_mantle_mass)] / planet_radius:.4f}")
 
     # Update the surface radius, interior radius, and mass in the hf_row
     hf_row["R_int"] = planet_radius