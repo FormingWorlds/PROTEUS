2872214f17eae6ff9a8bce1269fab523f3e21575
rdc49 <rdc49@cam.ac.uk>
2025-07-24 10:29:46 +0100

    fixing_instellation_bug

--- src/proteus/orbit/wrapper.py
+++ src/proteus/orbit/wrapper.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING
 import numpy as np
 
 from proteus.interior.common import Interior_t
-from proteus.utils.constants import AU, L_sun, const_G, secs_per_day
+from proteus.utils.constants import AU, L_sun, const_G, secs_per_day, R_sun
 
 if TYPE_CHECKING:
     from proteus import Proteus
@@ -142,7 +142,7 @@ def run_orbit(hf_row:dict, config:Config, dirs:dict, interior_o:Interior_t):
     if config.orbit.instellation_method == 'inst' and config.star.module == 'dummy':
         from proteus.star.dummy import calc_star_luminosity, get_star_radius
 
-        Lbol = calc_star_luminosity(config.star.dummy.Teff, get_star_radius(config))
+        Lbol = calc_star_luminosity(config.star.dummy.Teff, get_star_radius(config)*R_sun)
         S_earth = L_sun / (4 * np.pi * AU * AU)
         S_0 = config.orbit.instellationflux * S_earth
 
