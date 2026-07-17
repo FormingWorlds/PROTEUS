06705b5e7a771fe24f841942aa9feed2b5493ba9
Leoni Janssen <ljanssen@lumen.strw.leidenuniv.nl>
2026-07-17 15:28:41 +0200

    formatting fixed

--- tests/utils/test_terminate.py
+++ tests/utils/test_terminate.py
@@ -61,7 +61,7 @@ def _handler(cfg: Any, *, phi_global: float = 0.4) -> Any:
         'runtime': 10.0,
         'Time': 0.0,
         'M_vaps': 1.0e18,
-        'M_int': 1.0e24
+        'M_int': 1.0e24,
     }
 
     loops = {
