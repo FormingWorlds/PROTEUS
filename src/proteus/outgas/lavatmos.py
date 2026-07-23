6693c544ccb29b51bcb0c0bfc5a0f20afda8381e
Leoni Janssen <ljanssen@lumen.strw.leidenuniv.nl>
2026-07-23 14:41:33 +0200

    fixed typo iin logging statement lavatmos.py

--- src/proteus/outgas/lavatmos.py
+++ src/proteus/outgas/lavatmos.py
@@ -544,16 +544,17 @@ def run_vapourisation(dirs: dict, config: Config, hf_row: dict, first_iter: bool
                 + hf_row[vol + '_mol_liquid']
             )
 
-    Omass_after_outgas= 0.0
+    Omass_after_outgas = 0.0
     for e in element_list:
         log.debug('element frac:  %s,  %s', e, element_fracs[e])
         if e in input_eles:
             hf_row[e + '_kg_atm'] = (
-                                element_fracs[e] * M_atmo_new * species_lib[e].weight / mmw_elements
-                            )
+                element_fracs[e] * M_atmo_new * species_lib[e].weight / mmw_elements
+            )
         if e == 'O':
             Omass_after_outgas += (
-                element_fracs[e] * M_atmo_new * species_lib[e].weight / mmw_elements)
+                element_fracs[e] * M_atmo_new * species_lib[e].weight / mmw_elements
+            )
         else:
             if e not in gas_list:
                 hf_row[e + '_kg_atm'] = (
